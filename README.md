7fb3c66e69809116321db7f66f91d89fab7e814a
sairam.cherupally <sairamch04@gmail.com>
2018-06-10 13:31:12 +0530

    changes while experimenting

--- README.md
+++ README.md
@@ -4,7 +4,41 @@ callspy
 A simple tracing agent
 
 Build:
-gradlew jar
+gradle jar
 
 Run:
-java -DXbootclasspath/p:javassist.jar:callspy.jar -javaagent:callspy.jar YourClazz
+java  -javaagent:build/libs/callspy-0.1.jar com.zeroturnaround.callspy.Main
+
+Find the new bytecode in com/zeroturnaround/callspy/Main.class
+
+Decompiled classfile after Instumentating Main.java
+```java
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.zeroturnaround.callspy;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            Stack.push(); // added after instrumentation
+            Stack.log("com/zeroturnaround/callspy/Main.main");  // added after instrumentation
+            System.out.println("Hello from CallSpy!");
+            System.out.println("Usage: ");
+            System.out.println("   java -javaagent:/path/to/callspy.jar your.main.Clazz");
+            System.out.println("\nEnjoy! :-)");
+        } finally {  // added after instrumentation
+            Object var2 = null;
+            Stack.pop();
+        }
+
+    }
+}
+
+
+```
--- src/main/java/com/zeroturnaround/callspy/CallSpy.java
+++ src/main/java/com/zeroturnaround/callspy/CallSpy.java
@@ -10,52 +10,41 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class CallSpy implements ClassFileTransformer {
-  @Override
-  public byte[] transform(//region other parameters
-                          ClassLoader loader,
-                          String className,
-                          Class<?> classBeingRedefined,
-                          ProtectionDomain protectionDomain,
-                          //endregion
-                          byte[] classfileBuffer) throws IllegalClassFormatException {
+    @Override
+    public byte[] transform(//region other parameters
+                            ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+                            //endregion
+                            byte[] classfileBuffer) throws IllegalClassFormatException {
 
-    ClassPool cp = ClassPool.getDefault();
-    cp.importPackage("com.zeroturnaround.callspy");
+        ClassPool cp = ClassPool.getDefault();
+        cp.importPackage("com.zeroturnaround.callspy");
 
-    //region filter agent classes
-    // we do not want to profile ourselves
-    if (className.startsWith("com/zeroturnaround/callspy")) {
-      return null;
+        //region filter agent classes
+        if (!className.startsWith("com/zeroturnaround/callspy/Main")) {
+            return null;
+        }
+        CtClass ct = null;
+        try {
+            ct = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
+
+            CtMethod[] declaredMethods = ct.getDeclaredMethods();
+            for (CtMethod method : declaredMethods) {
+                //region instrument method
+                method.insertBefore(
+                        " { " + "Stack.push();" + "Stack.log(\"" + className + "." + method.getName() + "\"); " + "}");
+                method.insertAfter("{ Stack.pop(); }", true);
+                //endregion
+            }
+            ct.writeFile();
+            return ct.toBytecode();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            if (ct != null) {
+                //ct.detach();
+            }
+        }
+
+        return classfileBuffer;
     }
-    //endregion
-
-    //region filter out non-application classes
-    // Application filter. Can be externalized into a property file.
-    // For instance, profilers use blacklist/whitelist to configure this kind of filters
-    if (!className.startsWith("com/zt")) {
-      return classfileBuffer;
-    }
-    //endregion
-
-    try {
-      CtClass ct = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
-
-      CtMethod[] declaredMethods = ct.getDeclaredMethods();
-      for (CtMethod method : declaredMethods) {
-        //region instrument method
-          method.insertBefore(" { " +
-                  "Stack.push();" +
-                  "Stack.log(\"" + className + "." + method.getName() + "\"); " +
-                  "}");
-          method.insertAfter("{ Stack.pop(); }", true);
-        //endregion
-      }
-
-      return ct.toBytecode();
-    } catch (Throwable e) {
-      e.printStackTrace();
-    }
-
-    return classfileBuffer;
-  }
 }
